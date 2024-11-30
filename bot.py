09bd79b00f98b2c7e4e900444520be2399c4f4b0
Vonard <vonard000@gmail.com>
2024-11-30 14:22:39 +0300

    Correct code to follow PEP-8

--- bot.py
+++ bot.py
@@ -4,11 +4,12 @@ import random
 from dotenv import load_dotenv
 from comics import download_comic, check_comics
 
+
 def main():
     load_dotenv()
     bot = telegram.Bot(token=os.environ['TELEGRAM_BOT_TOKEN'])
     chat_id = os.environ['TG_CHAT_ID']
-    random_comic = random.randint(1,check_comics())
+    random_comic = random.randint(1, check_comics())
     download_comic(random_comic)
     try:
         with open(f'{random_comic}.png', 'rb') as file:
--- comics.py
+++ comics.py
@@ -1,11 +1,13 @@
 import requests
 
-def download_image(url, filename, api_key = ""):
+
+def download_image(url, filename, api_key=""):
     payload = {"api_key": api_key}
     response = requests.get(url, params=payload)
     response.raise_for_status()
     with open(f"{filename}", 'wb') as file:
         file.write(response.content)
+
 
 def download_comic(comic_number):
     url = f"https://xkcd.com/{comic_number}/info.0.json"
@@ -14,10 +16,9 @@ def download_comic(comic_number):
     comic = response.json()
     download_image(comic["img"], f"{comic_number}.png")
 
+
 def check_comics():
-    url = f"https://xkcd.com/info.0.json"
+    url = "https://xkcd.com/info.0.json"
     response = requests.get(url)
     response.raise_for_status()
     return response.json()["num"]
-
-
